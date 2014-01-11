bb2063ba66182a5a0056013441f9f234af7cfe4d
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-01-11 23:25:45 +0900

    Fix

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 __dir__=$(cd $(dirname $0);pwd)
 
 for file in .vim .vimrc .gitconfig .screenrc .tmux.conf; do
-  if [ -e ~/$file ]; then
-    echo Delete ~/$file
-    rm ~/$file
+  if [ -e $HOME/$file ]; then
+    echo Delete $HOME/$file
+    rm $HOME/$file
   fi
 
-  ln -vs $__dir__/$file ~/$file
+  ln -vs $__dir__/$file $HOME/$file
 done
 
 vim -c 'NeoBundleInstall!' -c 'q'
