44b838e703ff6d8401d993cf6f9539df566267d2
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-11-21 12:07:53 +0900

    Add brew.sh

---
+++ brew.sh
@@ -0,0 +1,53 @@
+brew update
+brew upgrade
+
+brew tap caskroom/cask
+brew tap caskroom/versions
+brew tap homebrew/dupes
+brew tap motemen/ghq
+brew tap peco/peco
+
+brew install brew-cask
+brew install ctags
+brew install curl
+brew install elasticsearch
+brew install ghq
+brew install git
+brew install go
+brew install heroku
+brew install hg
+brew install htop
+brew install hub
+brew install imagemagick
+brew install libiconv
+brew install mongodb
+brew install mysql
+brew install nginx
+brew install nvm
+brew install peco
+brew install phantomjs
+brew install pow
+brew install rbenv
+brew install ruby-build
+brew install rsync
+brew install sqlite
+brew install tig
+brew install tmux
+brew install tree
+brew install vim
+brew install watch
+brew install wget
+brew install --disable-etcdir zsh
+
+brew cask install google-chrome
+brew cask install java7
+brew cask install atom
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install gyazo
+brew cask install iterm2
+brew cask install skype
+brew cask install thunderbird-ja
+
+brew cleanup
+brew cask cleanup
