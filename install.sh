94bff39bb51416c9b63c4348eaf1705e165fcf81
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2022-07-03 23:06:38 +0000

    Add condition for GitHub Codespaces

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 __dir__=$(cd $(dirname $0);pwd)
 
-for file in .vim .vimrc .gitconfig .gitignore.global .screenrc .tigrc .tmux.conf; do
-  if [ -e $HOME/$file ]; then
-    echo Delete $HOME/$file
-    rm $HOME/$file
-  fi
+if [[ "$HOME" == "/home/codespace" ]]; then
+  # In GitHub Codespaces
+  for file in .tigrc .tmux.conf; do
+    if [ -e $HOME/$file ]; then
+      echo Delete $HOME/$file
+      rm $HOME/$file
+    fi
 
-  ln -vs $__dir__/$file $HOME/$file
-done
+    ln -vs $__dir__/$file $HOME/$file
+  done
+elif
+  # In the other environments
+  for file in .vim .vimrc .gitconfig .gitignore.global .screenrc .tigrc .tmux.conf; do
+    if [ -e $HOME/$file ]; then
+      echo Delete $HOME/$file
+      rm $HOME/$file
+    fi
+
+    ln -vs $__dir__/$file $HOME/$file
+  done
+fi
