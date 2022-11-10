14dc6a53a8f8c8eca079dd8c4fae5603643bd782
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2022-11-11 04:48:45 +0900

    Remove useless packages

--- brew.sh
+++ brew.sh
@@ -1,63 +1,28 @@
 brew update
 brew upgrade
 
-brew tap azure/draft
-brew tap caskroom/cask
-brew tap caskroom/versions
-brew tap homebrew/dupes
-brew tap homebrew/homebrew-php
 brew tap motemen/ghq
 brew tap tcnksm/ghr
 
 brew install awscli
-brew install brew-cask
-brew install cabal-install
-brew install cgdb
-brew install clisp
 brew install cmake
 brew install colordiff
-brew install composer
-brew install cpanminus
-brew install ctags
 brew install curl
-brew install draft
-brew install elasticsearch
-brew install erlang
-brew install fig
-brew install gdb
-brew install ghc
-brew install ghostscript
 brew install ghq
-brew install ghr
 brew install git
-brew install giter8
 brew install go
-brew install gts
 brew install graphviz
 brew install heroku
-brew install hg
 brew install htop
-brew install hub
 brew install imagemagick
-brew install jid
 brew install kubectl
 brew install kubectx
-brew install leiningen
 brew install libiconv
 brew install libxml2
 brew install llvm
-brew install lua
-brew install memcached
-brew install mongodb
-brew install mysql
-brew install nginx
-brew install nkf
 brew install nvm
 brew install peco
-brew install phantomjs
 brew install python3
-brew install postgresql
-brew install pow
 brew install pt
 brew install python
 brew install qt4
@@ -65,10 +30,6 @@ brew install rbenv
 brew install reattach-to-user-namespace
 brew install ruby-build
 brew install rsync
-brew install sbt
-brew install scala
-brew install sqlite
-brew install sshfs
 brew install stern
 brew install tig
 brew install tmux
@@ -77,29 +38,5 @@ brew install vim --override-system-vi --with-lua --with-python3
 brew install watch
 brew install wget
 brew install yarn
-brew install --disable-etcdir zsh
-
-brew cask install alfred
-brew cask install bathyscaphe
-brew cask install google-chrome
-brew cask install java7
-brew cask install atom
-brew cask install dropbox
-brew cask install firefox
-brew cask install google-chrome
-brew cask install gyazo
-brew cask install iterm2
-brew cask install minikube
-brew cask install robomongo
-brew cask install skype
-brew cask install thunderbird-ja
-brew cask install unity3d
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install yorufukurou
-brew cask install fastlane
-
-brew cask alfred link
 
 brew cleanup
-brew cask cleanup
